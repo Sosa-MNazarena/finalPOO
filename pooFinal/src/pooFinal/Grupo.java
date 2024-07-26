191697f4dfc737095f242d3fe3b477e72c7e60c5
SofiLeonar <sleonarholz@gmail.com>
2024-07-25 23:54:10 -0300

    feat: constructores clase Grupo
    Constructores - getters y setters - to String

--- pooFinal/src/pooFinal/Grupo.java
+++ pooFinal/src/pooFinal/Grupo.java
@@ -3,5 +3,32 @@ package pooFinal;
 public class Grupo {
 	private ArrayList<Equipo> equiposConformados;
     private String nombreGrupo;
+	public Grupo(ArrayList<Equipo> equiposConformados, String nombreGrupo) {
+		super();
+		this.equiposConformados = equiposConformados;
+		this.nombreGrupo = nombreGrupo;
+	}
+	
+	
+	public ArrayList<Equipo> getEquiposConformados() {
+		return equiposConformados;
+	}
+	public void setEquiposConformados(ArrayList<Equipo> equiposConformados) {
+		this.equiposConformados = equiposConformados;
+	}
+	public String getNombreGrupo() {
+		return nombreGrupo;
+	}
+	public void setNombreGrupo(String nombreGrupo) {
+		this.nombreGrupo = nombreGrupo;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Grupo [nombreGrupo=" + nombreGrupo + "]";
+	}
+    
+    
 
 }
