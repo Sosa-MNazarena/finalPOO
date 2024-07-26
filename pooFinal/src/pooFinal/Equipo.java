73965bcfe84ae0e736f09430d52d88df591ad7bf
SofiLeonar <sleonarholz@gmail.com>
2024-07-25 23:53:26 -0300

    feat: constructores clase Equipo
    Constructores - getters y setters - to String

--- pooFinal/src/pooFinal/Equipo.java
+++ pooFinal/src/pooFinal/Equipo.java
@@ -2,8 +2,46 @@ package pooFinal;
 
 public class Equipo {
 	private String name;
-    private int golesEquipo;
+	private int golesEquipo;
     private ArrayList<Partido> partidosJugados;
     private ArrayList<Jugador> jugadores;
+    
+    public Equipo(String name, int golesEquipo, ArrayList<Partido> partidosJugados, ArrayList<Jugador> jugadores) {
+		super();
+		this.name = name;
+		this.golesEquipo = golesEquipo;
+		this.partidosJugados = partidosJugados;
+		this.jugadores = jugadores;
+	}
 
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getGolesEquipo() {
+		return golesEquipo;
+	}
+	public void setGolesEquipo(int golesEquipo) {
+		this.golesEquipo = golesEquipo;
+	}
+	public ArrayList<Partido> getPartidosJugados() {
+		return partidosJugados;
+	}
+	public void setPartidosJugados(ArrayList<Partido> partidosJugados) {
+		this.partidosJugados = partidosJugados;
+	}
+	public ArrayList<Jugador> getJugadores() {
+		return jugadores;
+	}
+	public void setJugadores(ArrayList<Jugador> jugadores) {
+		this.jugadores = jugadores;
+	}
+	
+	 @Override
+		public String toString() {
+			return "Equipo [name=" + name + ", golesEquipo=" + golesEquipo + "]";
+		}
+	
 }
