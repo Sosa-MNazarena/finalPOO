cc519d7a797ba47848d410709cee2d46a2dfa591
Sosa-Nazarena <mariana.sosa@davinci.edu.ar>
2024-07-25 23:53:40 -0300

    feat: constructores clase Partido
    contructores, getters y setters, to string

--- pooFinal/src/pooFinal/Partido.java
+++ pooFinal/src/pooFinal/Partido.java
@@ -1,10 +1,54 @@
 package pooFinal;
 
+import java.util.Date;
+
 public class Partido {
 	private Date fecha;
 	private Equipo equipoLocal;
     private Equipo equipoVisitante;
     private Resultado resultado;
+    
+    public Partido(Date fecha, Equipo equipoLocal, Equipo equipoVisitante, Resultado resultado) {
+		super();
+		this.fecha = fecha;
+		this.equipoLocal = equipoLocal;
+		this.equipoVisitante = equipoVisitante;
+		this.resultado = resultado;
+	}
+    
+    
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public Equipo getEquipoLocal() {
+		return equipoLocal;
+	}
+	public void setEquipoLocal(Equipo equipoLocal) {
+		this.equipoLocal = equipoLocal;
+	}
+	public Equipo getEquipoVisitante() {
+		return equipoVisitante;
+	}
+	public void setEquipoVisitante(Equipo equipoVisitante) {
+		this.equipoVisitante = equipoVisitante;
+	}
+	public Resultado getResultado() {
+		return resultado;
+	}
+	public void setResultado(Resultado resultado) {
+		this.resultado = resultado;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Partido [fecha=" + fecha + ", equipoLocal=" + equipoLocal + ", equipoVisitante=" + equipoVisitante
+				+ ", resultado=" + resultado + "]";
+	}
+	
 	
 	
 
