954a2beada295ba023e90b5ea97f52bbf1371409
Sosa-Nazarena <mariana.sosa@davinci.edu.ar>
2024-07-25 23:53:59 -0300

    feat: constructores clase Resultado
    contructores, getters y setters, to string

--- pooFinal/src/pooFinal/Resultado.java
+++ pooFinal/src/pooFinal/Resultado.java
@@ -3,6 +3,28 @@ package pooFinal;
 public class Resultado {
 	private int golesLocal;
     private int golesVisitante;
+	public Resultado(int golesLocal, int golesVisitante) {
+		super();
+		this.golesLocal = golesLocal;
+		this.golesVisitante = golesVisitante;
+	}
+	public int getGolesLocal() {
+		return golesLocal;
+	}
+	public void setGolesLocal(int golesLocal) {
+		this.golesLocal = golesLocal;
+	}
+	public int getGolesVisitante() {
+		return golesVisitante;
+	}
+	public void setGolesVisitante(int golesVisitante) {
+		this.golesVisitante = golesVisitante;
+	}
+	@Override
+	public String toString() {
+		return "Resultado [golesLocal=" + golesLocal + ", golesVisitante=" + golesVisitante + "]";
+	}
 	
+    
 
 }
