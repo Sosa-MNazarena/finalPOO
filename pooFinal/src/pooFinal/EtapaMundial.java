85d39d038d03d2f5a53335daf3660d0d90de7c66
SofiLeonar <sleonarholz@gmail.com>
2024-07-25 23:53:46 -0300

    feat: constructores clase EtapaMundial
    Constructores - getters y setters - to String

--- pooFinal/src/pooFinal/EtapaMundial.java
+++ pooFinal/src/pooFinal/EtapaMundial.java
@@ -4,5 +4,38 @@ public class EtapaMundial {
 	private String descripcionEtapa;
     private ArrayList<Partido> partidos;
     private ArrayList<Equipo> equiposQueAvanzan;
+	public EtapaMundial(String descripcionEtapa, ArrayList<Partido> partidos, ArrayList<Equipo> equiposQueAvanzan) {
+		super();
+		this.descripcionEtapa = descripcionEtapa;
+		this.partidos = partidos;
+		this.equiposQueAvanzan = equiposQueAvanzan;
+	}
+	
+	
+	public String getDescripcionEtapa() {
+		return descripcionEtapa;
+	}
+	public void setDescripcionEtapa(String descripcionEtapa) {
+		this.descripcionEtapa = descripcionEtapa;
+	}
+	public ArrayList<Partido> getPartidos() {
+		return partidos;
+	}
+	public void setPartidos(ArrayList<Partido> partidos) {
+		this.partidos = partidos;
+	}
+	public ArrayList<Equipo> getEquiposQueAvanzan() {
+		return equiposQueAvanzan;
+	}
+	public void setEquiposQueAvanzan(ArrayList<Equipo> equiposQueAvanzan) {
+		this.equiposQueAvanzan = equiposQueAvanzan;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "EtapaMundial [descripcionEtapa=" + descripcionEtapa + "]";
+	}
 
+	
 }
