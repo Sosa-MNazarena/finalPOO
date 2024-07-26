6afd4cbed3e68d3fdf45f670f169aeff692f802d
SofiLeonar <sleonarholz@gmail.com>
2024-07-25 23:44:16 -0300

    feat: clase Equipo

---
+++ pooFinal/src/pooFinal/Equipo.java
@@ -0,0 +1,9 @@
+package pooFinal;
+
+public class Equipo {
+	private String name;
+    private int golesEquipo;
+    private ArrayList<Partido> partidosJugados;
+    private ArrayList<Jugador> jugadores;
+
+}
