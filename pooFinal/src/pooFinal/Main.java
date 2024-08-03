d19f9de05b9a1763c3d0d82936d46afb943639f9
SofiLeonar <sleonarholz@gmail.com>
2024-08-02 21:30:25 -0300

    feat: equipos

--- pooFinal/src/pooFinal/Main.java
+++ pooFinal/src/pooFinal/Main.java
@@ -3,8 +3,25 @@ package pooFinal;
 public class Main {
 
 	public static void main(String[] args) {
-		hola
-
+		Equipo qatar = new Equipo("Qatar", null);
+		Equipo ecuador = new Equipo("Ecuador", null);
+		Equipo paisesbajos = new Equipo("Paises Bajos", null);
+		Equipo senegai = new Equipo("Senegai", null);
+		
+		Equipo inglaterra = new Equipo("Inglaterra", null);
+		Equipo estadosunidos = new Equipo("Estados Unidos", null);
+		Equipo gales = new Equipo("Gales", null);
+		Equipo iran = new Equipo("Iran", null);
+		
+		Equipo argentina = new Equipo("Argentina", null);
+		Equipo polonia = new Equipo("Polonia", null);
+		Equipo mexico = new Equipo("Mexico", null);
+		Equipo arabiasaudita = new Equipo("Arabia Saudita", null);
+		
+		Equipo francia = new Equipo("Francia", null);
+		Equipo dinamarca = new Equipo("Dinamarca", null);
+		Equipo australia = new Equipo("Australia", null);
+		Equipo tunez = new Equipo("Tunez", null);
 	}
 
 }
